7a704c55a35bc9de91fe714e429f02b62277faaf
Romeu Reif <romeureif10@gmail.com>
2023-11-03 12:57:45 -0300

    desafio finalizado

---
+++ index.js
@@ -0,0 +1,23 @@
+function calcularWinxLoss(win, loss){
+    resultado = win - loss;
+    rank = "";
+    if(resultado <= 10){
+        rank = "Ferro";
+    } else if(resultado >= 11 && resultado <= 20){
+        rank = "Bronze";
+    } else if(resultado >= 21 && resultado <= 50){
+        rank = "Prata";
+    } else if(resultado >= 51 && resultado <= 80){
+        rank = "Ouro";
+    } else if(resultado >= 81 && resultado <= 90){
+        rank = "Diamante";
+    } else if(resultado => 91 && resultado <= 100){
+        rank = "Lendário";
+    } else {
+        rank = "Imortal";
+    }
+
+    console.log(`O Herói tem de saldo de ${resultado} está no nível de ${rank}`) ;
+}
+
+calcularWinxLoss(20,10);
